858725b56efca091c6a875a9d9929bc05245b713
Bohdan Diachenko <46127718+Bodyan-d@users.noreply.github.com>
2024-12-11 17:01:19 +0100

    Update shopping.py

--- shopping.py
+++ shopping.py
@@ -19,9 +19,7 @@ age_filter = st.sidebar.slider("Wiek klienta", int(data["Age"].min()), int(data[
 # Filtry kategorii dla poszczególnych wykresów
 category_filter_1 = st.sidebar.multiselect("Zakupy wg kategorii", data["Category"].unique(), data["Category"].unique())
 category_filter_2 = st.sidebar.multiselect("Średnia kwota zakupów wg sezonu", data["Season"].unique(), data["Season"].unique())
-category_filter_4 = st.sidebar.multiselect("rednia kwota zakupów wg wieku", data["Category"].unique(), data["Category"].unique())
 category_filter_5 = st.sidebar.multiselect("Procentowy udział kategorii w zakupach", data["Category"].unique(), data["Category"].unique())
-category_filter_6 = st.sidebar.multiselect("Średnia kwota zakupów wg dnia tygodnia", data["Category"].unique(), data["Category"].unique())
 
 # Filtruj dane na podstawie wieku i kategorii dla każdego wykresu
 filtered_data_1 = data[(data["Age"] >= age_filter[0]) & 
