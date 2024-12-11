e04b6af4d009ecd45cc04cf7a6f3795ce1e88400
Bohdan Diachenko <46127718+Bodyan-d@users.noreply.github.com>
2024-12-11 16:49:39 +0100

    Update shopping.py

--- shopping.py
+++ shopping.py
@@ -61,7 +61,6 @@ st.pyplot(fig)
 # Wykres 3: Liczba klientów wg wieku
 st.write("### Liczba klientów wg wieku")
 fig, ax = plt.subplots()
-filtered_data_3["Age"].hist(bins=20, ax=ax)
 ax.set_xlabel("Wiek")
 ax.set_ylabel("Liczba klientów")
 st.pyplot(fig)
